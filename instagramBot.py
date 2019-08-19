dc569d6de88a542a5125ffa3751d4776908d0972
sirlipski <38389415+sirlipski@users.noreply.github.com>
2019-08-19 15:12:10 +0000

    Add files via upload

--- instagramBot.py
+++ instagramBot.py
@@ -5,15 +5,12 @@ from io import BytesIO
 import os
 import sys
 import shutil
-from imageCreator import imageCreate, getAquote
 from datetime import datetime, timedelta
 import random
 import time as t
 
 username = 'tonyanevko'
 password = 'asdfghjkl123'
-image = '/Users/ivan.lipski/Desktop/newWork/pics/pic2.jpg'
-text = "Hello, Friend.\n\n\n.\n\n\nI'm Tonya Nevko, an experimental machine built\nto make your day a bit better\nby showing an inspiring quote.\n\n\n.\n\n\nAs long as my Creator's will remains,\nI will become more and more \ncomplex algorithm."
 caption = '#bot #coding #2019 #everydayimprove #testing'
 
 
@@ -25,12 +22,60 @@ def get_image(url):
     f = open('pic1.jpg','wb')
     f.write(requests.get(url).content)
     f.close() 
-    shutil.move('/Users/ivan.lipski/Desktop/newWork/pic1.jpg', '/Users/ivan.lipski/Desktop/newWork/pics/pic1.jpg')
+    #shutil.move('/Users/ivan.lipski/Desktop/newWork/pic1.jpg', '/Users/ivan.lipski/Desktop/newWork/pics/pic1.jpg')
 
 def upload_photo(image, text):
     photo_path = image
     caption = text
     InstagramAPI.uploadPhoto(photo_path, caption=caption)
+
+def deleteQuote(myline):
+    with open("movie_quotes_final.txt", "r") as f:
+        lines = f.readlines()
+    with open("movie_quotes_final.txt", "w") as f:
+        for line in lines:
+            if line.strip("\n\n") != myline:
+                f.write(line)
+
+def getAquote():
+    lines = open('movie_quotes_final.txt').read().split("\n")
+    myline = lines[random.randint(1,len(lines))]
+    deleteQuote(myline)
+    return myline.strip()
+
+def imageCreate(text,color,font,fontSize,name):
+    image = Image.new('RGB', (1000, 1000), color = color)
+
+    fnt = ImageFont.truetype(font, fontSize)
+
+    d = ImageDraw.Draw(image)
+    text_w, text_h = d.textsize(text, fnt)
+
+    lines = ['']*((int)(text_w/700)+1)
+
+    y_pos = (image.height-text_h*len(lines))/2
+
+    line_spacing = d.textsize('A', fnt)[1] + 6
+
+        
+    for i in range(len(lines)):
+        if d.textsize(text,fnt)[0] > image.width:
+            lastSpace = 30
+            while (d.textsize(text[0:lastSpace],fnt)[0] < image.width):
+                if text[lastSpace] != ' ':
+                    lastSpace-=1
+                else:
+                    break
+            lines[i] = text[0:lastSpace]
+            text = text[lastSpace+1:len(text)]
+        else:
+            lines[len(lines)-1] = text
+        if lines[i] != '':
+            d.text(((image.width - d.textsize(lines[i],fnt)[0])/2, y_pos), lines[i], font = fnt, fill=(textColor))
+            i+=1
+            y_pos += line_spacing
+    image.save(name)
+    return image
 
 #imageForTest = get_image('https://cdn.vox-cdn.com/thumbor/s6cI4NEU9hgaUFjXfyVPMmMc044=/cdn.vox-cdn.com/uploads/chorus_asset/file/3847870/11376655_479893395513491_201556343_n.0.jpg')
 
@@ -44,7 +89,7 @@ while 1:
     blue = random.randint(100,255)
 
     color = (red,green,blue)
-    font = '/Library/Fonts/Baskerville.ttc'
+    font = '/usr/share/fonts/truetype/msttcorefonts/Courier_New.ttf'
     fontSize = 72
     if(red*green*blue > 1000000):
         textColor = 'black'
